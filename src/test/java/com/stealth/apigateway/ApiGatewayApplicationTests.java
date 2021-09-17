675feaa1607d8db91742fb14e4a2d2aec91c0c2d
Lee Derek <derek@Lees-Mac-mini.local>
2021-09-17 20:17:24 +1200

    init

---
+++ src/test/java/com/stealth/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stealth.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
